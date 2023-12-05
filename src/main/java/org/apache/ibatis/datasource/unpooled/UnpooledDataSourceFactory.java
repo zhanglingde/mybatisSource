7dab64a018985cc4f11c6ab7e160a445a011d1e2
zhangling <390597591@qq.com>
2023-12-05 12:46:54 +0800

    基础支持层：数据源模块

--- src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -25,57 +25,67 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 
 /**
+ * 实现 DataSourceFactory 接口，非池化的 DataSourceFactory 实现类
+ *
  * @author Clinton Begin
  */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
 
-  private static final String DRIVER_PROPERTY_PREFIX = "driver.";
-  private static final int DRIVER_PROPERTY_PREFIX_LENGTH = DRIVER_PROPERTY_PREFIX.length();
+    private static final String DRIVER_PROPERTY_PREFIX = "driver.";
+    private static final int DRIVER_PROPERTY_PREFIX_LENGTH = DRIVER_PROPERTY_PREFIX.length();
 
-  protected DataSource dataSource;
+    protected DataSource dataSource;
 
-  public UnpooledDataSourceFactory() {
-    this.dataSource = new UnpooledDataSource();
-  }
-
-  @Override
-  public void setProperties(Properties properties) {
-    Properties driverProperties = new Properties();
-    MetaObject metaDataSource = SystemMetaObject.forObject(dataSource);
-    for (Object key : properties.keySet()) {
-      String propertyName = (String) key;
-      if (propertyName.startsWith(DRIVER_PROPERTY_PREFIX)) {
-        String value = properties.getProperty(propertyName);
-        driverProperties.setProperty(propertyName.substring(DRIVER_PROPERTY_PREFIX_LENGTH), value);
-      } else if (metaDataSource.hasSetter(propertyName)) {
-        String value = (String) properties.get(propertyName);
-        Object convertedValue = convertValue(metaDataSource, propertyName, value);
-        metaDataSource.setValue(propertyName, convertedValue);
-      } else {
-        throw new DataSourceException("Unknown DataSource property: " + propertyName);
-      }
+    public UnpooledDataSourceFactory() {
+        this.dataSource = new UnpooledDataSource();
     }
-    if (driverProperties.size() > 0) {
-      metaDataSource.setValue("driverProperties", driverProperties);
-    }
-  }
 
-  @Override
-  public DataSource getDataSource() {
-    return dataSource;
-  }
-
-  private Object convertValue(MetaObject metaDataSource, String propertyName, String value) {
-    Object convertedValue = value;
-    Class<?> targetType = metaDataSource.getSetterType(propertyName);
-    if (targetType == Integer.class || targetType == int.class) {
-      convertedValue = Integer.valueOf(value);
-    } else if (targetType == Long.class || targetType == long.class) {
-      convertedValue = Long.valueOf(value);
-    } else if (targetType == Boolean.class || targetType == boolean.class) {
-      convertedValue = Boolean.valueOf(value);
+    @Override
+    public void setProperties(Properties properties) {
+        Properties driverProperties = new Properties();
+        // 创建 dataSource 对应的 MetaObject 对象，其中是BeanWrapper
+        MetaObject metaDataSource = SystemMetaObject.forObject(dataSource);
+        // 遍历 properties 属性，初始化到 driverProperties 和 MetaObject 中
+        for (Object key : properties.keySet()) {
+            String propertyName = (String) key;
+            // 初始化到 driverProperties 中
+            if (propertyName.startsWith(DRIVER_PROPERTY_PREFIX)) {
+                String value = properties.getProperty(propertyName);
+                driverProperties.setProperty(propertyName.substring(DRIVER_PROPERTY_PREFIX_LENGTH), value);
+            // 如果该属性在UnpooledDataSource中有setter方法，则初始化到 MetaObject 中
+            } else if (metaDataSource.hasSetter(propertyName)) {
+                String value = (String) properties.get(propertyName);
+                Object convertedValue = convertValue(metaDataSource, propertyName, value);
+                // 将数据设置到UnpooledDataSource中去
+                metaDataSource.setValue(propertyName, convertedValue);
+            } else {
+                throw new DataSourceException("Unknown DataSource property: " + propertyName);
+            }
+        }
+        if (driverProperties.size() > 0) {
+            // 设置 driverProperties 到 MetaObject 中
+            metaDataSource.setValue("driverProperties", driverProperties);
+        }
     }
-    return convertedValue;
-  }
+
+    @Override
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    private Object convertValue(MetaObject metaDataSource, String propertyName, String value) {
+        Object convertedValue = value;
+        // 获得该属性的 setting 方法的参数类型
+        Class<?> targetType = metaDataSource.getSetterType(propertyName);
+        // 转化
+        if (targetType == Integer.class || targetType == int.class) {
+            convertedValue = Integer.valueOf(value);
+        } else if (targetType == Long.class || targetType == long.class) {
+            convertedValue = Long.valueOf(value);
+        } else if (targetType == Boolean.class || targetType == boolean.class) {
+            convertedValue = Boolean.valueOf(value);
+        }
+        return convertedValue;
+    }
 
 }
